7882da65d1fbf06991364b1e6c4c25b2070e14a3
Ephraim Glick <ephraim.glick@gmail.com>
2018-02-12 14:19:11 +0000

    Add sub-description inside app header

---
+++ app/components/Header/AppSubDescription.js
@@ -0,0 +1,17 @@
+/**
+*
+* AppSubDescription
+*
+*/
+
+import styled from 'styled-components';
+
+export default styled.div`
+  padding: 0.5em 2em 0 .25em;
+  padding: 0 25px 0 25px;
+  background-color: LightGrey;
+  color: #41ADDD;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: bold;
+  font-size: 18px;
+`;
